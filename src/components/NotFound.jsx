07489cd2a94e4be5fc7dec28f898cb6d04084d8e
alamin <alaminsf40@gmail.com>
2024-12-18 22:08:07 +0600

    new

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="mx-auto border h-[200px] w-full bg-white shadow-lg flex items-center justify-center">
+      <h2 className="text-2xl text-center">There is no product available</h2>
+    </div>
+  );
+}
--- src/components/newArrivals/NewArrivals.jsx
+++ src/components/newArrivals/NewArrivals.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { ProductContext } from "../../context";
 import Loader from "../Loader/Loader";
+import NotFound from "../NotFound";
 import ArrivalsTop from "./ArrivalsTop";
 import Card from "./Card";
 import Cart from "./Cart";
@@ -72,13 +73,18 @@ export default function NewArrivals() {
         <div>
           <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 lg:max-w-7xl lg:px-8">
-              <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+              <div
+                className={`mt-6 grid grid-cols-1 gap-x-6 gap-y-10 ${
+                  updatedItem.length > 0 ? "sm:grid-cols-2 lg:grid-cols-4" : ""
+                } xl:gap-x-8`}
+              >
                 {/* <!-- Card --> */}
                 {loading ? (
                   <Loader />
-                ) : (
-                  updatedItem.length > 0 &&
+                ) : updatedItem.length > 0 ? (
                   updatedItem.map((list) => <Card key={list.id} list={list} />)
+                ) : (
+                  <NotFound />
                 )}
               </div>
             </div>
